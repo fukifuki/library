5f6aa467aeabcd6ef4d8b7f9f0fa074a23e83f97
Rade Krstanovic <radekrstanovic@gmail.com>
2018-03-18 20:38:27 +0100

    Define Book model

---
+++ script.js
@@ -0,0 +1,15 @@
+// MODELS
+// Book model
+function Book (title, author, pages, isRead=false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+Book.prototype.info = function () {
+  let info = this.title + ' by ' + this.author + ', ' + this.pages + ' pages';
+  return info += this.isRead ? ' (read)' : ' (not read yet)';
+}
+
+
