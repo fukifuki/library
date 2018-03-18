3ac51e6fea0127745c8b126a5c796ec82c24b8ba
Rade Krstanovic <radekrstanovic@gmail.com>
2018-03-18 21:03:47 +0100

    Define books getter

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Book
 function Book (title, author, pages, isRead=false) {
+
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+
 }
 
-Book.prototype.info = function () {
+Book.prototype.info = function () {  
   let info = this.title + ' by ' + this.author + ', ' + this.pages + ' pages';
   return info += this.isRead ? ' (read)' : ' (not read yet)';
 }
@@ -16,3 +18,10 @@ const library = []
 
 
 
+// Library controller
+const libraryController = {
+
+  getBooks: function () {
+    return library;
+  }
+}
